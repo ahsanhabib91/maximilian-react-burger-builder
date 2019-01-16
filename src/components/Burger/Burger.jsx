9cf430b3454d44663523f5da092f439f0fd168c0
ahsanhabib91 <habibibnakther@gmail.com>
2019-01-16 18:13:42 +0600

    Working on Burger

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 
+import classes from "./Burger.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
+
 const burger = props => {
-  return <div>asdf</div>;
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    console.log(igKey, props.ingredients[igKey]);
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient key={igKey + i} type={igKey} />;
+    });
+  });
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends React.Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 2,
+      meat: 1
+    },
+    totalPrice: 4,
+    purchasable: false,
+    purchasing: false
+  };
   render() {
     return (
       <React.Fragment>
-        <div>Burger</div>
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </React.Fragment>
     );
